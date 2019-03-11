4ff106d4968ed51ae879c7d35ed6a8cd17f742fb
bensamazingsoft <ben.mouginot@gmail.com>
2019-03-11 18:00:09 +0100

    11/03

--- PixelImageCreator/src/main/java/com/ben/pixcreator/gui/keyhandler/KeyHandler.java
+++ PixelImageCreator/src/main/java/com/ben/pixcreator/gui/keyhandler/KeyHandler.java
@@ -9,14 +9,15 @@ import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
 
 public class KeyHandler implements EventHandler<KeyEvent> {
+	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(KeyHandler.class);
 
 	@Override
 	public void handle(KeyEvent event) {
-		log.debug("Handling " + event.getCode());
+		// log.debug("Handling " + event.getCode());
 		if (event.getEventType().getName().equals("KEY_PRESSED") &&
 				event.getCode().isWhitespaceKey()) {
-			log.debug("KEY_PRESSED -> event.getCode() : " + event.getCode());
+			// log.debug("KEY_PRESSED -> event.getCode() : " + event.getCode());
 			GuiFacade.getInstance().getActiveTab().togglePanMode();
 
 		}
