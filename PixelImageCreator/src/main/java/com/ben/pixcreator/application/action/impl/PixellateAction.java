ae4cca52a21f71286e0a5ab8e0df68b2f4368a37
bensamazingsoft <ben.mouginot@gmail.com>
2019-03-13 16:42:21 +0100

    impl pixellate menu (average/pick center)

--- PixelImageCreator/src/main/java/com/ben/pixcreator/application/action/impl/PixellateAction.java
+++ PixelImageCreator/src/main/java/com/ben/pixcreator/application/action/impl/PixellateAction.java
@@ -1,10 +1,13 @@
 
 package com.ben.pixcreator.application.action.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.ben.pixcreator.application.action.IAction;
+import com.ben.pixcreator.application.color.ColorUtils;
 import com.ben.pixcreator.application.color.rgb.ColorRGB;
 import com.ben.pixcreator.application.executor.Executor;
 import com.ben.pixcreator.application.image.PixImage;
@@ -17,17 +20,25 @@ import com.ben.pixcreator.gui.facade.GuiFacade;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
 
 public class PixellateAction implements IAction {
 
-	private final PixImage	image;
-	private final PixTab	tab;
-	final GuiFacade			gui	= GuiFacade.getInstance();
+	final GuiFacade gui = GuiFacade.getInstance();
 
-	public PixellateAction(PixTab tab, PixImage activeImage) {
+	private final PixImage		image;
+	private final PixTab		tab;
+	private final actionType	actionType;
+
+	public PixellateAction(actionType type, PixTab tab, PixImage activeImage) {
 
 		this.image = activeImage;
 		this.tab = tab;
+		this.actionType = type;
+	}
+
+	public enum actionType {
+		AVERAGE, CENTERPICK;
 	}
 
 	@Override
@@ -46,11 +57,18 @@ public class PixellateAction implements IAction {
 		for (int x = 0; x < canvas.getWidth(); x += xCellSize) {
 			for (int y = 0; y < canvas.getHeight(); y += yCellSize) {
 
-				ColorRGB average = pickCellCenterPixelColor(reader, x, y, xCellSize, yCellSize);
+				Color average = Color.WHITE;
+				if (actionType == actionType.CENTERPICK) {
+					average = pickCellCenterPixelColor(reader, x, y, xCellSize, yCellSize);
+				}
+
+				if (actionType == actionType.AVERAGE) {
+					average = averageColor(reader, x, y, xCellSize, yCellSize);
+				}
 
 				// exclude BG color
-				if (!average.getFxColor().equals(gui.getBackgroundColor())) {
-					grid.put(new Coord(x / xCellSize, y / yCellSize), average);
+				if (!average.equals(gui.getBackgroundColor())) {
+					grid.put(new Coord(x / xCellSize, y / yCellSize), new ColorRGB(average));
 				}
 
 			}
@@ -80,34 +98,30 @@ public class PixellateAction implements IAction {
 		return snap;
 	}
 
-	// private ColorRGB averageColor(PixelReader reader, int x, int y, int
-	// xCellSize, int yCellSize)
-	// {
-	//
-	// Color average = Color.WHITE;
-	// List<Color> colors = new ArrayList<>();
-	//
-	// for (int i = 0; i < xCellSize; i++)
-	// {
-	// for (int j = 0; j < yCellSize; j++)
-	// {
-	//
-	// colors.add(reader.getColor(x + i, y + j));
-	//
-	// }
-	// }
-	//
-	// average = ColorUtils.averageColor(colors);
-	//
-	// return new ColorRGB(average);
-	// }
+	private Color averageColor(PixelReader reader, int x, int y, int xCellSize, int yCellSize) {
 
-	private ColorRGB pickCellCenterPixelColor(PixelReader reader, int x, int y, int xCellSize, int yCellSize) {
+		Color average = Color.WHITE;
+		List<Color> colors = new ArrayList<>();
+
+		for (int i = 0; i < xCellSize; i++) {
+			for (int j = 0; j < yCellSize; j++) {
+
+				colors.add(reader.getColor(x + i, y + j));
+
+			}
+		}
+
+		average = ColorUtils.averageColor(colors);
+
+		return average;
+	}
+
+	private Color pickCellCenterPixelColor(PixelReader reader, int x, int y, int xCellSize, int yCellSize) {
 
 		int centerX = x + xCellSize / 2;
 		int centerY = y + yCellSize / 2;
 
-		return new ColorRGB(reader.getColor(centerX, centerY));
+		return reader.getColor(centerX, centerY);
 	}
 
 }
