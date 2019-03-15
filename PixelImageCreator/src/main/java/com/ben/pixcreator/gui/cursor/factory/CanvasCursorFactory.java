de2a6e2421d2ed19a226dae43b08db4c2cfe691c
bensamazingsoft <ben.mouginot@gmail.com>
2019-03-15 17:31:21 +0100

    impl pick cursor

--- PixelImageCreator/src/main/java/com/ben/pixcreator/gui/cursor/factory/CanvasCursorFactory.java
+++ PixelImageCreator/src/main/java/com/ben/pixcreator/gui/cursor/factory/CanvasCursorFactory.java
@@ -1,3 +1,4 @@
+
 package com.ben.pixcreator.gui.cursor.factory;
 
 import com.ben.pixcreator.application.context.AppContext;
@@ -5,50 +6,71 @@ import com.ben.pixcreator.application.context.AppContext;
 import javafx.scene.Cursor;
 import javafx.scene.ImageCursor;
 
-public class CanvasCursorFactory implements CursorFactory {
+public class CanvasCursorFactory implements CursorFactory
+{
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.ben.pixcreator.gui.cursor.factory.CursorFactory#getCursor(java.lang.
-	 * Boolean)
-	 */
-	@Override
-	public Cursor getCursor(Boolean panMode) {
+      /*
+       * (non-Javadoc)
+       * 
+       * @see com.ben.pixcreator.gui.cursor.factory.CursorFactory#getCursor(java.lang. Boolean)
+       */
+      @Override
+      public Cursor getCursor(Boolean panMode)
+      {
 
-		if (panMode) {
+	    if (panMode)
+	    {
 
-		}
+	    }
 
-		switch (AppContext.getInstance().getCurrTool()) {
-		case DRAW:
-			double[] X = { 16d, 9d, 27d };
-			double[] Y = { 2d, 13d, 13d };
-			ImageCursor cursor = new ImageCursor(makeImage("draw.png", X, Y, 3), 16, 2);
-			return cursor;
-		case MOVE:
-			break;
-		case NONE:
-			break;
-		case PAN:
-			break;
-		case PICK:
-			break;
-		case RESIZE:
-			break;
-		case SELECT:
-			break;
-		case ZOOMIN:
-			break;
-		case ZOOMOUT:
-			break;
-		default:
-			break;
+	    switch (AppContext.getInstance().getCurrTool())
+	    {
+	    case DRAW:
+		  double[] X = {
+			      16d,
+			      9d,
+			      23d };
+		  double[] Y = {
+			      2d,
+			      13d,
+			      13d };
+		  ImageCursor cursor = new ImageCursor(makeImage("draw.png", X, Y, 3), 16, 2);
+		  return cursor;
+	    case MOVE:
+		  break;
+	    case NONE:
+		  break;
+	    case PAN:
+		  break;
+	    case PICK:
 
-		}
+		  double[] Xpick = {
+			      25,
+			      15,
+			      18,
+			      72 };
+		  double[] yPick = {
+			      28,
+			      15,
+			      18,
+			      28 };
+		  ImageCursor pickCursor = new ImageCursor(makeImage("pick.png", Xpick, yPick, 4), 35, 28);
 
-		return Cursor.DEFAULT;
-	}
+		  return pickCursor;
+	    case RESIZE:
+		  break;
+	    case SELECT:
+		  break;
+	    case ZOOMIN:
+		  break;
+	    case ZOOMOUT:
+		  break;
+	    default:
+		  break;
+
+	    }
+
+	    return Cursor.DEFAULT;
+      }
 
 }
